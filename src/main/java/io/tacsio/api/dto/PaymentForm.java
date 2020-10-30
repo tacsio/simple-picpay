cd3caea0bd982eefc710b42b0ab7f8cdb0aa65b6
Tarcisio Coutinho <tcs5cin@gmail.com>
2020-10-30 20:02:26 -0300

    Tests

--- src/main/java/io/tacsio/api/dto/PaymentForm.java
+++ src/main/java/io/tacsio/api/dto/PaymentForm.java
@@ -8,7 +8,7 @@ import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 
 public class PaymentForm {
-    @Positive
+    @Positive(message = "Payment value must be greater than 0.")
     public BigDecimal value;
     @NotNull
     public Long payer;
--- src/test/java/io/tacsio/fixtures/PaymentFixtures.java
+++ src/test/java/io/tacsio/fixtures/PaymentFixtures.java
@@ -10,29 +10,6 @@ import io.tacsio.service.dto.TransactionValitadorResponse;
 
 public class PaymentFixtures {
 
-  public static PaymentForm userToUser(double ammount) {
-
-    List<User> users = User.list("type", UserType.DEFAULT);
-    if (users.size() < 2) {
-      throw new IllegalStateException("There is no 2 users registered in database to make this payment.");
-    }
-
-    long idPayer = users.get(0).getId();
-    long idPayee = users.get(1).getId();
-    BigDecimal value = BigDecimal.valueOf(ammount);
-
-    return of(idPayer, idPayee, value);
-  }
-
-  public static PaymentForm of(Long idPayer, Long idPayee, BigDecimal value) {
-    PaymentForm form = new PaymentForm();
-    form.payer = idPayer;
-    form.payee = idPayee;
-    form.value = value;
-
-    return form;
-  }
-
   public static TransactionValitadorResponse authorizedResponse() {
     TransactionValitadorResponse response = new TransactionValitadorResponse();
     response.message = "Autorizado";
@@ -43,7 +20,53 @@ public class PaymentFixtures {
   public static TransactionValitadorResponse unauthorizedResponse() {
     TransactionValitadorResponse response = new TransactionValitadorResponse();
     response.message = "Denied";
-    
+
     return response;
   }
+
+  public static PaymentForm of(Long idPayer, Long idPayee, double value) {
+    PaymentForm form = new PaymentForm();
+    form.payer = idPayer;
+    form.payee = idPayee;
+    form.value = BigDecimal.valueOf(value);
+
+    return form;
+  }
+
+  public static PaymentForm userToUser(double ammount) {
+
+    List<User> users = User.list("type", UserType.DEFAULT);
+    if (users.size() < 2) {
+      throw new IllegalStateException("There is no 2 users registered in database to make this payment.");
+    }
+
+    long idPayer = users.get(0).getId();
+    long idPayee = users.get(1).getId();
+
+    return of(idPayer, idPayee, ammount);
+  }
+
+  public static PaymentForm userToShopkeeper(double ammount) {
+    User client = User.find("type", UserType.DEFAULT).firstResult();
+    User shoopkeeper = User.find("type", UserType.SHOPKEEPER).firstResult();
+
+    return of(client.getId(), shoopkeeper.getId(), ammount);
+  }
+
+  public static PaymentForm shopkeeperToUser(double ammount) {
+    User client = User.find("type", UserType.DEFAULT).firstResult();
+    User shoopkeeper = User.find("type", UserType.SHOPKEEPER).firstResult();
+
+    return of(shoopkeeper.getId(), client.getId(), ammount);
+  }
+
+  public static PaymentForm shopkeeperToShopkeeper(double ammount) {
+    List<User> shoopkeepers = User.list("type", UserType.SHOPKEEPER);
+    if (shoopkeepers.size() < 2) {
+      throw new IllegalStateException("There is no 2 shoopkeepers registered in database to make this payment.");
+    }
+
+    return of(shoopkeepers.get(0).getId(), shoopkeepers.get(1).getId(), ammount);
+  }
+
 }
